c77a60b1c0fa5b38f5c5821494ee8b102b2365f3
KaiwenJon <kaiwenjon23@gmail.com>
2021-07-04 19:53:25 +0800

    fix: update argument name

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from src.drawer.SearchTimeToNodeNum import drawTimeToSearch_N_Node
 #### see description in the following modules ####
 
 drawDistribution(nodeNum=50000,loadFactor=5, drawing=True)
-drawLFandLambda(N=10000)
-drawRatioLLandRBTree(TT=4, drawing=True)
-drawTimeToConstructMap(10000)
-drawTimeToSearch_N_Node(10000)
+# drawLFandLambda(nodeNum=10000)
+# drawRatioLLandRBTree(TT=4, drawing=True)
+# drawTimeToConstructMap(10000)
+# drawTimeToSearch_N_Node(10000)
 
